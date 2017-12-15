9409c3028ff03582762acf5b01f8ef3649b6ddab
Brian Peek <brian@brianpeek.com>
2017-12-15 01:03:58 -0500

    Initial import of MobileApps game

---
+++ MobileAppsRacer/Assets/Scripts/AzureMobileServiceClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.WindowsAzure.MobileServices;
+
+public static class AzureMobileServiceClient
+{
+    // Use mozroots / cert-syc to update your Unity Mono certificate store!
+    private const string backendUrl = "MOBILE_APP_URL";
+    private static MobileServiceClient client;
+
+    public static MobileServiceClient Client
+    {
+        get
+        {
+            if (client == null)
+            {
+#if UNITY_ANDROID
+                // Android builds fail at runtime due to missing GZip support, so build a handler that uses Deflate for Android
+                var handler = new HttpClientHandler { AutomaticDecompression = DecompressionMethods.Deflate };
+                client = new MobileServiceClient(backendUrl, handler);
+#else
+                Client = new MobileServiceClient(backendUrl);
+#endif
+            }
+
+            return client;
+        }
+    }
+}
---
+++ MobileAppsRacer/Assets/Scripts/TestClientConnection.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using Microsoft.WindowsAzure.MobileServices;
+
+public class TestClientConnection : MonoBehaviour
+{
+    void Start()
+    {
+        Task.Run(TestTableConnection);
+    }
+
+    private async Task TestTableConnection()
+    {
+        Debug.Log("Testing Mobile App setup");
+        var table = AzureMobileServiceClient.Client.GetTable<CrashInfo>();
+
+        Debug.Log("Testing ToListAsync...");
+        await TestToListAsync(table);
+
+        Debug.Log("Testing InsertAsync...");
+        await TestInsertAsync(table);
+
+        Debug.Log("Testing DeleteAsync...");
+        await TestDeleteAsync(table);
+
+        Debug.Log("All testing complete.");
+    }
+
+    private async Task TestInsertAsync(IMobileServiceTable<CrashInfo> table)
+    {
+        try
+        {
+            var allEntries = await TestToListAsync(table);
+            var initialCount = allEntries.Count();
+
+            await table.InsertAsync(new CrashInfo { X = 1, Y = 2, Z = 3 });
+
+            allEntries = await TestToListAsync(table);
+            var newCount = allEntries.Count();
+
+            Debug.Assert(newCount == initialCount + 1, "InsertAsync failed!");
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    private async Task<List<CrashInfo>> TestToListAsync(IMobileServiceTable<CrashInfo> table)
+    {
+        try
+        {
+            var allEntries = await table.ToListAsync();
+            Debug.Assert(allEntries != null, "ToListAsync failed!");
+            return allEntries;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    private async Task TestDeleteAsync(IMobileServiceTable<CrashInfo> table)
+    {
+        var allEntries = await TestToListAsync(table);
+
+        foreach (var item in allEntries)
+        {
+            try
+            {
+                await table.DeleteAsync(item);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        allEntries = await TestToListAsync(table);
+
+        Debug.Assert(allEntries.Count() == 0, "DeleteAsync failed!");
+    }
+}
